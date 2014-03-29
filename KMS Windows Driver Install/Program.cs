a49ac2b1389e61d20e107b1955b3b704081b9133
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-03-28 19:26:16 -0600

    Añadido instalador de Driver para Windows

---
+++ KMS Windows Driver Install/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
+using System.Security;
+
+namespace KMS.Desktop.Windows.DriverInstall {
+    class Program {
+        static DirectoryInfo InstallDir
+            = (new DirectoryInfo(Application.StartupPath));
+
+        [DllImport("DIFXApi.dll", CharSet = CharSet.Unicode)]
+        static extern Int32 DriverPackagePreinstall(string DriverPackageInfPath, Int32 Flags);
+
+        static void AddCertificate(StoreName store, string name) {
+            X509Store certStore = new X509Store(
+                store,
+                StoreLocation.LocalMachine
+            );            
+            X509Certificate2 cert
+                = new X509Certificate2(
+                    InstallDir.FullName + @"\Certificates\" + name
+                );
+
+            certStore.Open(OpenFlags.ReadOnly);
+            X509Certificate2Collection certSearchResult
+                = certStore.Certificates.Find(X509FindType.FindByThumbprint, cert.Thumbprint, true);
+
+            if ( certSearchResult.Count > 0 )
+                return;
+
+            certStore.Close();
+
+            certStore.Open(OpenFlags.ReadWrite);
+            certStore.Add(cert);
+
+            certStore.Close();
+        }
+        
+        static void Main(string[] args) {
+            DialogResult dialogResult
+                = DialogResult.Cancel;
+
+            do {
+                try {
+                    AddCertificate(StoreName.AuthRoot, "KmsRoot.cer");
+                    AddCertificate(StoreName.TrustedPublisher, "KmsSoftware.cer");
+                    
+                    dialogResult
+                        = DialogResult.Cancel;
+                } catch ( Exception ex ) {
+                    dialogResult
+                        =  MessageBox.Show(
+                            string.Format(
+                                "{0}\n\n{1}",
+                                KMSWindowsDriverInstall.FailedCertMessage,
+                                ex.Message
+                            ),
+                            KMSWindowsDriverInstall.FailedTitle,
+                            MessageBoxButtons.RetryCancel,
+                            MessageBoxIcon.Error
+                        );
+
+                    if ( dialogResult == DialogResult.Cancel )
+                        return;
+                }
+            } while ( dialogResult == DialogResult.Retry );
+
+            int result
+                = 0;
+            string exception
+                = "";
+
+            do {
+                try {
+                    result
+                        =  DriverPackagePreinstall(InstallDir.FullName + @"\Driver\kmsdevice.inf", 0);
+                    exception
+                        = "Preinstaller #" + result.ToString();
+                } catch ( Exception ex ) {
+                    result
+                        = -1;
+                    exception
+                        = ex.Message;
+                }
+                
+                if ( result == 0 ) {
+                    break;
+                } else {
+                    dialogResult
+                        =  MessageBox.Show(
+                            string.Format(
+                                "{0}\n\n{1}",
+                                KMSWindowsDriverInstall.FailedMessage,
+                                exception
+                            ),
+                            KMSWindowsDriverInstall.FailedTitle,
+                            MessageBoxButtons.RetryCancel,
+                            MessageBoxIcon.Error
+                        );
+
+                    if ( dialogResult == DialogResult.Cancel )
+                        return;
+                }
+            } while ( dialogResult == DialogResult.Retry && result != 0 );
+
+            if ( ! args.Contains("-Silent") ) {
+                MessageBox.Show(
+                    KMSWindowsDriverInstall.SuccessMessage,
+                    KMSWindowsDriverInstall.SucessTitle,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+        }
+    }
+}
---
+++ KMS Windows Driver Install/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KMS Desktop Windows Certificate and Driver Installer")]
+[assembly: AssemblyDescription("KMS Desktop Root and Code Signing Certificate and Driver Installer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KMS Invent")]
+[assembly: AssemblyProduct("KMS Desktop")]
+[assembly: AssemblyCopyright("Copyright © 2014 - Sinuhé Coronel Salinas")]
+[assembly: AssemblyTrademark("KMS")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d424badd-0ac4-4bc7-aeb9-02d0b9110282")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

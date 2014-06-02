1d8bbba032ab4cda4613de97ed80f2e445fab91f
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-06-01 23:35:10 -0500

    Añadida tipografía Open Sans a KMS Desktop.

---
+++ KMS Desktop/Program.WindowsFonts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMS.Desktop {
+    #if !__MonoCS__
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
+
+    static partial class Program {
+        [DllImport("gdi32", EntryPoint = "AddFontResource")]
+        static extern int Gdi_AddFontResource(string lpFilename);
+
+        [DllImport("gdi32", EntryPoint = "RemoveFontResource")]
+        static extern bool Gdi_RemoveFontResource(string lpFileName);
+
+        private static String WorkingDirectory =
+            Path.GetDirectoryName(Application.ExecutablePath);
+
+        private static List<String> m_loadedFonts = 
+            new List<String>();
+
+        private static void InitializeFontLoading() {
+            String[] fonts = System.IO.Directory.GetFiles(WorkingDirectory, "*.ttf");
+
+            foreach ( var font in fonts ) {
+                Gdi_AddFontResource(font);
+                m_loadedFonts.Add(font);
+            }
+
+            Application.ApplicationExit += Application_ApplicationExit;
+        }
+
+        static void Application_ApplicationExit(object sender, EventArgs e) {
+            foreach ( var font in m_loadedFonts )
+                Gdi_RemoveFontResource(font);
+        }
+    }
+    #endif
+}
